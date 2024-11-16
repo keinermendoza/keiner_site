288992ffc17c65d42f7fa655a212dc94f3cab54f
keiner <keinermendoza@gmail.com>
2024-11-15 21:23:28 -0400

    adding new logo

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path, re_path
 from . import views
-from . import assets_view
 from django.views.generic import RedirectView, TemplateView
 app_name = 'core'
 
 urlpatterns = [
     # path("", views.HomePage.as_view(), name="home"),
     path("", views.simple_home, name="home"),
-    path("signed-css", assets_view.css_view, name="css_view"),
     # path("", TemplateView.as_view(template_name="pages/home/new_desing.html"), name="home"),
     path("about", views.AboutPage.as_view(), name="about"),
     path("contact-me", views.ContactFormPartialView.as_view(), name="contact_me"),
